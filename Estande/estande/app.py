e4327ef8748cabfebc8e068192c3c5408c1b6c13
LucasGatica <30784336+LucasGatica@users.noreply.github.com>
2024-06-21 12:50:40 -0300

    estande - funcionando o sistema de fechar

--- Estande/estande/app.py
+++ Estande/estande/app.py
@@ -10,28 +10,36 @@ games_info = [
         "categories": ["Esportes", "Multiplayer", "Competitivo"],
         "description": "FIFA 2023 é o mais recente título da popular série de jogos de futebol da EA Sports. Compita em ligas do mundo todo.",
         "executable_path": "C:\\Program Files (x86)\\Steam\\steamapps\\common\\BOT.vinnik Chess Early USSR Championships\\bot4_release\\Bot.vinnik Chess Early USSR Championships.exe",
-        "image": "/static/images/fifa.jpg"
+        "image": "/static/images/fifa.jpg",
+        "qr_code":  " ",
+        "average": ""
     },
     {
         "name": "Stardew Valley",
         "categories": ["RPG", "Casual", "Indie"],
         "description": "Stardew Valley é um RPG de simulação onde você herda uma antiga fazenda e começa uma nova vida no campo.",
         "executable_path": "C:\\path\\to\\stardew_valley.exe",
-        "image": "/static/images/stardew_valley.jpg"
+        "image": "/static/images/stardew_valley.jpg",
+        "qr_code":  " ",
+        "average": ""
     },
     {
         "name": "Guilty Gear",
         "categories": ["Luta", "Arcade", "Competitivo"],
         "description": "Guilty Gear é um jogo de luta com gráficos impressionantes e uma jogabilidade rápida e emocionante.",
         "executable_path": "C:\\path\\to\\guilty_gear.exe",
-        "image": "/static/images/guilty_gear.jpg"
+        "image": "/static/images/guilty_gear.jpg",
+        "qr_code":  " ",
+        "average": ""
     },
     {
         "name": "F1 2024",
         "categories": ["Corrida", "Simulação", "Esportes"],
         "description": "F1 2024 traz a emoção das corridas de Fórmula 1 com gráficos realistas e jogabilidade intensa.",
         "executable_path": "C:\\path\\to\\f1_2024.exe",
-        "image": "/static/images/f1_2024.jpg"
+        "image": "/static/images/f1_2024.jpg",
+        "qr_code":  " ",
+        "average": ""
     }
 ]
 
--- Estande/estande/game_launcher.py
+++ Estande/estande/game_launcher.py
@@ -1,9 +1,29 @@
 import os
 import subprocess
+import threading
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+
 
 def launch_game(executable_path):
     if os.path.exists(executable_path):
-        subprocess.Popen([executable_path], shell=True)
+        process = subprocess.Popen([executable_path], shell=True)
+
+        logging.info(f'Jogo iniciado: {executable_path}')
+
+        timer = threading.Timer(10.0, terminate_process_by_name, [os.path.basename(executable_path)])
+        timer.start()
+
         return True
     else:
         return False
+
+
+def terminate_process_by_name(process_name):
+    logging.info(f'Encerrando o jogo {process_name} após 30 segundos')
+    try:
+        subprocess.run(["taskkill", "/F", "/IM", process_name], check=True)
+        logging.info(f'Jogo {process_name} encerrado com sucesso')
+    except subprocess.CalledProcessError as e:
+        logging.error(f'Erro ao encerrar o jogo {process_name}: {e}')

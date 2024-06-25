96e3a9860e1afaae9c87dcdd926da3b3da70e910
Maria Eduarda Santellano Maidana Carvalho <santellanoeduarda@gmail.com>
2024-06-25 18:40:34 -0300

    Atualiza js modal Qrcode

--- Estande/estande/static/js/script.js
+++ Estande/estande/static/js/script.js
@@ -81,6 +81,23 @@ document.addEventListener('DOMContentLoaded', (event) => {
             updateGamepadStatus();
         }
     }
+
+    //Função para abrir o modal do qrcode
+    
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("btn-review");
+    var span = document.getElementsByClassName("close")[0];
+    btn.onclick = function() {
+    modal.style.display = "block";
+    }
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    }
 });
 
 
